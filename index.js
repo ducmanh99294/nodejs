154f551235f582776d922fc36527e61311cd6fe6
ducmanh <127844553+ducmanh99294@users.noreply.github.com>
2024-08-31 09:06:09 +0700

    Delete index.js

--- index.js
+++
@@ -1,8 +0,0 @@
-const http = require('node:http');
-
-http.createServer((request, response) => {
-    response.writeHead(200, {"Content-Type" : "text/html; charset=UTF-8"})
-    response.write('<h1>hello mạnh</h1>')
-    response.end();
-  // magic happens here!
-}).listen(8080);
